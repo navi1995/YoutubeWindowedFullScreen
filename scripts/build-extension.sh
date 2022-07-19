5e15fff366a198aad5968011e1c003dba2c7ec1e
Navi Jador <navi1995@hotmail.com>
2022-07-19 15:47:32 +1000

    Testing CICD

--- scripts/build-extension.sh
+++ scripts/build-extension.sh
@@ -1,18 +1,33 @@
 #!/bin/sh
-rm -rf sitemap
+rm -rf deployments
 mkdir deployments
 version=$(cat ./extension/manifest.json | egrep -o "([0-9]{1,}\.)+[0-9]{1,}")
 zip_version="${version/./_}"
 firefox_output=./deployments/Firefox.zip
 chrome_output=./deployments/Chrome.zip
 manifest_location=./deployments/manifest.json
+sed_option=
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+	sed_option='-i ""'
+else
+	sed_option='-i'
+fi
 
 zip -j $firefox_output ./extension/* -x *.json
 yes | cp ./extension/manifest-firefox.json $manifest_location
-sed -i "" 's/\"version\":.*/\"version\": "'${version}'",/g' "${manifest_location}"
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  sed -i '' -e 's/\"version\":.*/\"version\": "'${version}'",/g' "${manifest_location}"
+else
+  sed -i -e 's/\"version\":.*/\"version\": "'${version}'",/g' "${manifest_location}"
+fi
 zip -j $firefox_output $manifest_location
 
 zip -j $chrome_output ./extension/* -x *.json
 yes | cp ./extension/manifest-chrome.json $manifest_location
-sed -i "" 's/\"version\":.*/\"version\": "'${version}'",/g' "${manifest_location}"
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  sed -i '' -e 's/\"version\":.*/\"version\": "'${version}'",/g' "${manifest_location}"
+else
+  sed -i -e 's/\"version\":.*/\"version\": "'${version}'",/g' "${manifest_location}"
+fi
 zip -j $chrome_output $manifest_location
