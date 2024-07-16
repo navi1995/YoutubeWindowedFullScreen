dad743c5ef1b08c205c735f3bbb9c1b3cc2a70bd
Navi Jador <navi1995@hotmail.com>
2024-07-16 15:37:48 +1000

    Fix for new layout

--- extension/YouTube.fullbrowser.js
+++ extension/YouTube.fullbrowser.js
@@ -80,7 +80,7 @@
 	function enterFullBrowser() {
 		var original = document.getElementById("original-size");
 		var newControl = document.getElementById("full-size");
-		var watchContainer = document.querySelector("ytd-watch") || document.querySelector("ytd-watch-flexy") || document.querySelector("#player");
+		var watchContainer = document.querySelector("ytd-watch-grid") || document.querySelector("ytd-watch") || document.querySelector("ytd-watch-flexy") || document.querySelector("#player");
 
 		resizeEvent = window.matchMedia("(max-width: 882px)");
 		resizeEvent.addListener(handleMediaQuery);
@@ -94,9 +94,7 @@
 			isTheatreMode = true;
 		}
 
-		document.getElementById("movie_player").classList.add("updated-full-mode");
 		document.body.classList.add("updated-full-mode");
-		document.getElementsByClassName("html5-main-video")[0].classList.add("updated-full-mode");
 		original.style.display = "none";
 		newControl.style.display = "inline-block";
 		newControl.innerHTML = "<svg width=\"20\" height=\"28\" viewBox=\"0 0 1792 1792\" xmlns=\"http://www.w3.org/2000/svg\" class=\"svg-container\"><path d=\"M896 960v448q0 26-19 45t-45 19-45-19l-144-144-332 332q-10 10-23 10t-23-10l-114-114q-10-10-10-23t10-23l332-332-144-144q-19-19-19-45t19-45 45-19h448q26 0 45 19t19 45zm755-672q0 13-10 23l-332 332 144 144q19 19 19 45t-19 45-45 19h-448q-26 0-45-19t-19-45v-448q0-26 19-45t45-19 45 19l144 144 332-332q10-10 23-10t23 10l114 114q10 10 10 23z\" style=\"fill: white;\"></path></svg>";
@@ -109,9 +107,7 @@
 		var newControl = document.getElementById("full-size");
 
 		isFullMode = false;
-		document.getElementById("movie_player").classList.remove("updated-full-mode");
 		document.body.classList.remove("updated-full-mode");
-		document.getElementsByClassName("html5-main-video")[0].classList.remove("updated-full-mode");
 		original.style.display = "inline-block";
 		newControl.style.display = "inline-block";
 		newControl.innerHTML = "<svg width=\"20\" height=\"28\" viewBox=\"0 0 1792 1792\" xmlns=\"http://www.w3.org/2000/svg\" class=\"svg-container\"><path d=\"M883 1056q0 13-10 23l-332 332 144 144q19 19 19 45t-19 45-45 19h-448q-26 0-45-19t-19-45v-448q0-26 19-45t45-19 45 19l144 144 332-332q10-10 23-10t23 10l114 114q10 10 10 23zm781-864v448q0 26-19 45t-45 19-45-19l-144-144-332 332q-10 10-23 10t-23-10l-114-114q-10-10-10-23t10-23l332-332-144-144q-19-19-19-45t19-45 45-19h448q26 0 45 19t19 45z\" style=\"fill: white;\"></path></svg>";
